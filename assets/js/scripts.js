053060160d370572662f3274365244c5067b4326
Raghuveer999 <rpanchagnula@traveltripper.com>
2017-08-22 12:49:29 +0530

    home page slider

---
+++ assets/js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	var height = $(window).height();  //getting windows height
+	jQuery('#homePageMainSlider').css('height',height+'px');   //and setting height of carousel
+	jQuery('#accomPageSlider').css('height',height+'px');   //and setting height of carousel
+});
