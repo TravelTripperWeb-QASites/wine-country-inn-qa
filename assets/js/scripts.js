f9723554cb394e63e056d9db476ca9beef7395bf
Raghuveer999 <rpanchagnula@traveltripper.com>
2017-08-23 18:02:51 +0530

    scroll header complete

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,4 +2,20 @@ $(document).ready(function(){
 	var height = $(window).height();  //getting windows height
 	jQuery('#homePageMainSlider').css('height',height+'px');   //and setting height of carousel
 	jQuery('#accomPageSlider').css('height',height+'px');   //and setting height of carousel
+
+
+	setScrollHeader();  
+  	$(window).scroll(function (event) {
+	    setScrollHeader();
+	});
 });
+
+function setScrollHeader(){
+	var scroll = $(window).scrollTop();
+    if(scroll == 0){
+    	$(".navbar-section").removeClass("scroll-header");
+    } else {
+    	$(".navbar-section").addClass("scroll-header");
+    }	
+}
+
